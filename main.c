e5d31e9486c3094af17a1f5a84c754e2db15215c
Rovva <rovva@hostarn.se>
2021-12-17 14:11:19 +0100

    Init again

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * D0003E_Lab_1.c
+ *
+ * Created: 2021-12-17 14:10:40
+ * Author : chris
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
