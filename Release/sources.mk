d130583e97899ef91a7a30055078e581eef85959
Christoffer Rova <rovva@hostarn.se>
2022-01-21 21:39:21 +0100

    Fixed writeLong

---
+++ Release/sources.mk
@@ -0,0 +1,16 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+C_SRCS := 
+O_SRCS := 
+FLASH_IMAGE := 
+ELFS := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+. \
+
--- main.c
+++ main.c
@@ -1,19 +1,24 @@
-// LCD Enable (LCDEN) & Low Power Waveform (LCDAB)
-LCDCRA = (1<<LCDEN) | (1<<LCDAB) | (0<<LCDIF) | (0<<LCDIE) | (0<<LCDBL);
-
-// external asynchronous clock source (LCDCS), 1/3 bias (LCD2B), 1/4 duty (LCDMUX1:0), 25 segments enabled (LCDPM2:0)
-LCDCRB = (1<<LCDCS) | (0<<LCD2B) | (1<<LCDMUX1) | (1<<LCDMUX0) | (1<<LCDPM2) | (1<<LCDPM1) | (1<<LCDPM0);
-
-// prescaler setting N=16 (LCDPS2:0), clock divider setting D=8 (LCDCD2:0)
-LCDFRR = (0<<LCDPS2) | (0<<LCDPS1) | (0<<LCDPS0) | (1<<LCDCD2) | (1<<LCDCD1) | (1<<LCDCD0);
-
-// drive time 300 microseconds (LCDDC2:0), contrast control voltage 3.35 V (LCDCC3:0)
-LCDCCR = (0<<LCDDC2) | (0<<LCDDC1) | (0<<LCDDC0) | (1<<LCDCC3) | (1<<LCDCC2) | (1<<LCDCC1) | (1<<LCDCC0);
 
 #include <avr/io.h>
 
+
+void LCDinit() {
+	// LCD Enable (LCDEN) & Low Power Waveform (LCDAB)
+	LCDCRA = (1<<LCDEN) | (1<<LCDAB) | (0<<LCDIF) | (0<<LCDIE) | (0<<LCDBL);
+
+	// external asynchronous clock source (LCDCS), 1/3 bias (LCD2B), 1/4 duty (LCDMUX1:0), 25 segments enabled (LCDPM2:0)
+	LCDCRB = (1<<LCDCS) | (0<<LCD2B) | (1<<LCDMUX1) | (1<<LCDMUX0) | (1<<LCDPM2) | (1<<LCDPM1) | (1<<LCDPM0);
+
+	// prescaler setting N=16 (LCDPS2:0), clock divider setting D=8 (LCDCD2:0)
+	LCDFRR = (0<<LCDPS2) | (0<<LCDPS1) | (0<<LCDPS0) | (1<<LCDCD2) | (1<<LCDCD1) | (1<<LCDCD0);
+
+	// drive time 300 microseconds (LCDDC2:0), contrast control voltage 3.35 V (LCDCC3:0)
+	LCDCCR = (0<<LCDDC2) | (0<<LCDDC1) | (0<<LCDDC0) | (1<<LCDCC3) | (1<<LCDCC2) | (1<<LCDCC1) | (1<<LCDCC0);
+
+}
+
 unsigned short digitLookUp(unsigned char c) {
-	unsigned short binary = 0;
+	unsigned long binary = 0;
 	if(c == 48) {
 		// 0 needs A, B, C, D, F, E, K, P
 		/*
@@ -22,7 +27,7 @@ unsigned short digitLookUp(unsigned char c) {
 			LCDDRx+10 = 0101
 			LCDDRx+15 = 0101
 		*/
-		binary = 0b1001010101010101;
+		binary = 0b0001010101010001;
 	} else if(c == 49) {
 		// 1 needs B, C
 		/*
@@ -40,7 +45,7 @@ unsigned short digitLookUp(unsigned char c) {
 			LCDDRx+10 = 1110
 			LCDDRx+15 = 0001
 		*/
-		binary = 0b0001000111100001
+		binary = 0b0001000111100001;
 	} else if(c == 51) {
 		// 3 needs A, B, L, G, C, D
 		/*
@@ -59,7 +64,7 @@ unsigned short digitLookUp(unsigned char c) {
 			LCDDRx+15 = 0000
 		*/
 		binary = 0b0000010110110000;
-	} else if(c = 53) {
+	} else if(c == 53) {
 		// 5 needs A, F, G, L, C, D
 		/*
 			LCDDRx = 0001
@@ -111,23 +116,23 @@ unsigned short digitLookUp(unsigned char c) {
 }
 
 void writeChar(unsigned char letter, unsigned int position) {
-	unsigned short digitBinary;
+	unsigned long digitBinary;
 	unsigned char nibble_0 = 0, nibble_1 = 0, nibble_2 = 0, nibble_3 = 0, oldValue = 0;
 	
 	digitBinary = digitLookUp(letter);
 	
 	if(position == 0) {
 		// Extract nibble from the binary output of binLookup
-		¨//               0001 0101 1011 0000
+		//               0001 0101 1011 0000
 		//           0000 1111
 		nibble_0 = 0b00001111 & (digitBinary >> 12);
-		¨//          0001 0101 1011 0000
+		//          0001 0101 1011 0000
 		//           0000 1111
 		nibble_1 = 0b00001111 & (digitBinary >> 8);
-		¨//     0001 0101 1011 0000
+		//     0001 0101 1011 0000
 		//           0000 1111
 		nibble_2 = 0b00001111 & (digitBinary >> 4);
-		¨//0001 0101 1011 0000
+		//0001 0101 1011 0000
 		//           0000 1111
 		nibble_3 = 0b00001111 & digitBinary;
 				
@@ -158,9 +163,96 @@ void writeChar(unsigned char letter, unsigned int position) {
 		nibble_3 = 0b11110000 & (digitBinary << 4);
 		
 		oldValue = 0b00001111 & LCDDR0;
-		
-		LCDDR0 = oldValue | nibble_3;
+		LCDDR0 = oldValue | nibble_0;
+
+		oldValue = 0b00001111 & LCDDR5;
+		LCDDR5 = oldValue | nibble_1;
+
+		oldValue = 0b00001111 | LCDDR10;
+		LCDDR10 = oldValue | nibble_2;
+
+		oldValue = 0b00001111 | LCDDR15;
+		LCDDR15 = oldValue | nibble_3;
+	} else if(position == 2) {
+		nibble_0 = 0b00001111 & (digitBinary >> 12);
+		nibble_1 = 0b00001111 & (digitBinary >> 8);
+		nibble_2 = 0b00001111 & (digitBinary >> 4);
+		nibble_3 = 0b00001111 & digitBinary;
+
+		oldValue = 0b11110000 & LCDDR1;
+		LCDDR1 = oldValue | nibble_0;
+		oldValue = 0b11110000 & LCDDR6;
+		LCDDR6 = oldValue | nibble_1;
+		oldValue = 0b11110000 & LCDDR11;
+		LCDDR11 = oldValue | nibble_2;
+		oldValue = 0b11110000 & LCDDR16;
+		LCDDR16 = oldValue | nibble_3;
+	} else if(position == 3) {
+		nibble_0 = 0b11110000 & (digitBinary >> 8);
+		nibble_1 = 0b11110000 & (digitBinary >> 4);
+		nibble_2 = 0b11110000 & digitBinary;
+		nibble_3 = 0b11110000 & (digitBinary << 4);
+
+		oldValue = 0b00001111 & LCDDR1;
+		LCDDR1 = oldValue | nibble_0;
+		oldValue = 0b00001111 & LCDDR6;
+		LCDDR6 = oldValue | nibble_1;
+		oldValue = 0b00001111 & LCDDR11;
+		LCDDR11 = oldValue | nibble_2;
+		oldValue = 0b00001111 & LCDDR16;
+		LCDDR16 = oldValue | nibble_3;
+	} else if(position == 4) {
+		nibble_0 = 0b00001111 & (digitBinary >> 12);
+		nibble_1 = 0b00001111 & (digitBinary >> 8);
+		nibble_2 = 0b00001111 & (digitBinary >> 4);
+		nibble_3 = 0b00001111 & digitBinary;
+
+		oldValue = 0b11110000 & LCDDR2;
+		LCDDR2 = oldValue | nibble_0;
+		oldValue = 0b11110000 & LCDDR7;
+		LCDDR7 = oldValue | nibble_1;
+		oldValue = 0b11110000 & LCDDR12;
+		LCDDR12 = oldValue | nibble_2;
+		oldValue = 0b11110000 & LCDDR17;
+		LCDDR17 = oldValue | nibble_3;
+	} else if(position == 5) {
+		nibble_0 = 0b11110000 & (digitBinary >> 8);
+		nibble_1 = 0b11110000 & (digitBinary >> 4);
+		nibble_2 = 0b11110000 & digitBinary;
+		nibble_3 = 0b11110000 & (digitBinary << 4);
+
+		oldValue = 0b00001111 & LCDDR2;
+		LCDDR2 = oldValue | nibble_0;
+		oldValue = 0b00001111 & LCDDR7;
+		LCDDR7 = oldValue | nibble_1;
+		oldValue = 0b00001111 & LCDDR12;
+		LCDDR12 = oldValue | nibble_2;
+		oldValue = 0b00001111 & LCDDR17;
+		LCDDR17 = oldValue | nibble_3;
+	} else {
+		return;
 	}
+}
+
+void writeLong(long value) {
+	char array[31];
+	long temp = value;
+	// n is needed to show the digits correctly (reverse)
+	int n = 5;
+	// For loop using modulus and division to extract all the digits in the long int
+	for(int i = 0; i < 31; i++) {
+		array[i] = (temp%10) + '0';
+		temp /= 10;
+		// if the temp variable is less than 1, break loop
+		if(temp < 1) {
+			break;
+		}
+	}
+	// For loop to show the 6 least significant digits
+ 	for(int i = 0; i <= 5; i++) {
+ 		writeChar(array[i], n);
+		n--;
+ 	}
 }
 
 int main(void)
@@ -169,6 +261,8 @@ int main(void)
 	CLKPR  = 0x80;
 	CLKPR  = 0x00;
 	
+	LCDinit();
+	writeLong(123456);
     while (1) 
     {
     }
